f846a1adcd1a0657ea81d24819014dbe3a3e8e22
NandawteK <nandawtek@gmail.com>
2022-08-09 10:44:38 +0200

    add recipe detail

---
+++ app/recipe/views.py
@@ -0,0 +1,22 @@
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+from core.models import Recipe
+from recipe import serializers
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.RecipeDetailSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user).order_by('-id')
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return serializers.RecipeSerializer
+
+        return self.serializer_class
